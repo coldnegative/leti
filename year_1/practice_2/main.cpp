e62b956dec04ea1a14a31894e1c5d30351295b7a
coldnegative <vologinmp@gmail.com>
2020-12-07 15:51:53 +0300

    practice_2

--- year_1/practice_2/main.cpp
+++ year_1/practice_2/main.cpp
@@ -1,25 +1,22 @@
 #include <iostream>
-#include <ctime>
 #include <chrono>
 
 using namespace std;
+using namespace std::chrono;
 
 void menu_print() {
         cout << "1. Создать массив.\n";
         cout << "2. Показать массивы.\n";
         cout << "3. Сортировка пузырьком.\n";
         cout << "4. Сортировка перемешиванием.\n";
-        cout << "5. Сортировка расческой.\n";
-        cout << "6. Сортировка вставками.\n";
-        cout << "7. Быстрая сортировка.\n";
-        cout << "8. Сортировка Шелла.\n";
-        cout << "9. Найти минимальный и максимальный элементы.\n";
-        cout << "10. Найти среднее арифметическое минимального и максимального элементов, а также индексы всех элементов, которые равны этому значению, и их количество.\n";
-        cout << "11. Найти количество элементов меньше a.\n";
-        cout << "12. Найти количество элементов больше b.\n";
-        cout << "13. Проверка наличия числа x в отсортированном массиве, а также сравнение перебора и бинарного поиска.\n";
-        cout << "14. Поменять 2 элемента местами.\n";
-        cout << "15. Выход.\n";
+        cout << "5. Сортировка вставками.\n";
+        cout << "6. Найти минимальный и максимальный элементы.\n";
+        cout << "7. Найти среднее арифметическое минимального и максимального элементов, а также индексы всех элементов, которые равны этому значению, и их количество.\n";
+        cout << "8. Найти количество элементов меньше a.\n";
+        cout << "9. Найти количество элементов больше b.\n";
+        cout << "10. Проверка наличия числа x в отсортированном массиве, а также сравнение перебора и бинарного поиска.\n";
+        cout << "11. Поменять 2 элемента местами.\n";
+        cout << "12. Выход.\n";
         cout << ">";
 }
 
@@ -41,26 +38,36 @@ void array_copy(int from[], int *to, int N) {
 }
 
 void sort_bubble(int *array, int N) {
+    auto startTime = system_clock::now();
     for (int i=0; i < N - 1; i++)
         for (int j=0; j < N - i - 1; j++)
             if (array[j] > array[j + 1]) {
                 array[j] ^= array[j + 1] ^= array[j] ^= array[j + 1];
             }
+    auto endTime = system_clock::now();
+    duration<double> elapsed = endTime - startTime;
+    cout << std::fixed << "Результат сортировки пузырьком: " << elapsed.count() << " сек.\n";
 }
 
 void sort_shaker(int *array, int N) {
     int i;
+    auto startTime = system_clock::now();
     for (i = 0; i < N;) {
         for (int j= i + 1; j < N; j++)
             if (array[j] < array[j - 1]) { array[j] ^= array[j - 1] ^= array[j] ^= array[j - 1]; }
         N--;
         for (int k = N - 1; k > i; k--)
             if (array[k] < array[k - 1]) { array[k] ^= array[k - 1] ^= array[k] ^= array[k - 1]; }
-        i++; }
+        i++; 
+    }
+    auto endTime = system_clock::now();
+    duration<double> elapsed = endTime - startTime;
+    cout << std::fixed << "Результат сортировки перемешиванием: " << elapsed.count() << " сек.\n";
 }
 
 void sort_insert(int *array, int N) {
     int k, j;
+    auto startTime = system_clock::now();
     for (int i = 1; i < N; i++) {
         k = array[i];
         j = i - 1;
@@ -70,72 +77,23 @@ void sort_insert(int *array, int N) {
         }
         array[j + 1] = k;
     }
-}
-/*
-void sort_comb(int *array, int N) {
-	const double k = 1.247330950103979;
-	int step = N;
-	while (step > 1) {
-		for (int i = 0; i + step < N; i++)
-			if (array[i] > array[i + step]) {
-                array[i] ^= array[i + step] ^= array[i] ^= array[i + step];
-            }
-		step /= k;
-	}
-	bool b = true;
-	while (b) {
-		b = false;
-		for (int i = 0; i + 1 < N; i++) {
-			if (array[i] > array[i + step]) {
-				array[i] ^= array[i + 1] ^= array[i] ^= array[i + 1];
-				b = true;
-			}
-		}
-	}
+    auto endTime = system_clock::now();
+    duration<double> elapsed = endTime - startTime;
+    cout << std::fixed << "Результат сортировки вставками: " << elapsed.count() << " сек.\n";
 }
 
-void sort_quick(int *array, int N) {
-    if (r - l <= 1) return;
-	int z = *(l + (r - l) / 2);
-	int* ll = l, *rr = r - 1;
-	while (ll <= rr) {
-		while (*ll < z) ll++;
-		while (*rr > z) rr--;
-		if (ll <= rr) {
-			swap(*ll, *rr);
-			ll++;
-			rr--;
-		}
-	}
-	if (l < rr) sort_quick(l, rr + 1);
-	if (ll < r) sort_quick(ll, r);
-}
-
-void sort_shell(int *array, int N) {
-    int sz = r - l;
-	int step = sz / 2;
-	while (step >= 1) {
-		for (int *i = l + step; i < r; i++) {
-			int *j = i;
-			int *diff = j - step;
-			while (diff >= l && *diff > *j) {
-				swap(*diff, *j);
-				j = diff;
-				diff = j - step;
-			}
-		}
-		step /= 2;
-	}
-}
-*/
 void minmax(int array[], int N, bool check) {
     int min, max;
     min = max = array[0];
+    auto startTime = system_clock::now();
     for (int i = 1; i < N; i++)
         if (array[i] > max) {max = array[i];}
         else if (array[i] < min) {min = array[i];}
+    auto endTime = system_clock::now();
+    duration<double> elapsed = endTime - startTime;
     if (check == 0) {
         cout << "Минимальный элемент: " << min << "\nМаксимальный элемент: " << max << "\n";
+        cout << std::fixed << "Затраченное на поиск время: " << elapsed.count() << " сек.\n";
     } else {
         int avg = ((min + max) / 2), count = 0;
         cout << "Среднее арифметическое минимального и максимального элементов: " << avg << "\n";
@@ -172,27 +130,40 @@ void array_find(int *array, int N, int X, int check) {
         break;
     case 3: {
         int left = 0, right = N - 1, mid;
+        auto startTimeLin = system_clock::now();
         for (int i = 0; i < N; i++)
             if (array[i] == X) {
                 flag = true;
                 break;
             }
+        auto endTimeLin = system_clock::now();
+        duration<double> elapsedLinear = endTimeLin - startTimeLin;
         if (flag) {
-            printf("Число %d найдено методом перебора за d секунд.\n", X);
+            printf("Число %d найдено методом перебора за %f сек.\n", X, elapsedLinear.count());
         } else {
-            printf("Число %d не найдено методом перебора за d секунд.\n", X);
+            printf("Число %d не найдено методом перебора за %f сек.\n", X, elapsedLinear.count());
         }
         flag = false;
+        auto startTimeBin = system_clock::now();
         while ((left <= right) && (flag != true)) {
             mid = (left + right) / 2;
             if (array[mid] == X) { flag = true; }
             else if (array[mid] > X) { right = mid - 1; }
             else { left = mid + 1; };
         }
+        auto endTimeBin = system_clock::now();
+        duration<double> elapsedBinary = endTimeBin - startTimeBin;
         if (flag) {
-            printf("Число %d найдено бинарным поиском за d секунд.\n", X);
+            printf("Число %d найдено бинарным поиском за %f сек.\n", X, elapsedBinary.count());
         } else {
-            printf("Число %d не найдено бинарным поиском за d секунд.\n", X);
+            printf("Число %d не найдено бинарным поиском за %f сек.\n", X, elapsedBinary.count());
+        }
+        if (elapsedLinear > elapsedBinary) {
+            printf("Поиск методом перебора был быстрее на %f сек.\n", (elapsedLinear.count() - elapsedBinary.count()));
+        } else if (elapsedBinary > elapsedLinear) {
+            printf("Бинарный поиск был быстрее на %f сек.\n", (elapsedLinear.count() - elapsedBinary.count()));
+        } else {
+            cout << "Оба алгоритма поиска показали одинаковое время.\n";
         }
         break;
         }
@@ -200,11 +171,15 @@ void array_find(int *array, int N, int X, int check) {
 }
 
 void array_swap(int *array, int A, int B) {
+    auto startTime = system_clock::now();
     array[A] ^= array[B] ^= array[A] ^= array[B];
+    auto endTime = system_clock::now();
+    duration<double> elapsed = endTime - startTime;
+    cout << std::fixed << "Перестановка элементов завершена за: " << elapsed.count() << " сек.\n";
 }
 
 int main() {
-    const int n = 10;
+    const int n = 100;
     int choice, a = 0, b = 0, x = 0, swapA = 0, swapB = 0;
     int array_unsorted[n]={}, array_sorted[n]={};
     do {
@@ -228,42 +203,30 @@ int main() {
             goto explicit_label;
         case 5:
             array_copy(array_unsorted, array_sorted, n);
-            //sort_comb(array_sorted, n);
-            goto explicit_label;
-        case 6:
-            array_copy(array_unsorted, array_sorted, n);
             sort_insert(array_sorted, n);
             goto explicit_label;
-        case 7:
-            array_copy(array_unsorted, array_sorted, n);
-            //sort_quick(array_sorted, n);
-            goto explicit_label;
-        case 8:
-            array_copy(array_unsorted, array_sorted, n);
-            //sort_shell(array_sorted, n);
-            goto explicit_label;
-        case 9:
+        case 6:
             minmax(array_unsorted, n, 0);
             break;
-        case 10:
+        case 7:
             minmax(array_unsorted, n, 1);
             break;
-        case 11:
+        case 8:
             cout << "Введите a: ";
             cin >> a;
             array_find(array_sorted, n, a, 1);
             break;
-        case 12:
+        case 9:
             cout << "Введите b: ";
             cin >> b;
             array_find(array_sorted, n, b, 2);
             break;
-        case 13:
+        case 10:
             cout << "Введите x: ";
             cin >> x;
             array_find(array_sorted, n, x, 3);
             break;
-        case 14:
+        case 11:
             cout << "Введите первый индекс: ";
             cin >> swapA;
             cout << "Введите второй индекс: ";
@@ -278,7 +241,7 @@ int main() {
                 cout << "Введена некорректная пара индексов.\n";
             }
             break;
-        case 15:
+        case 12:
             break;
         default:
             explicit_label:
@@ -288,7 +251,7 @@ int main() {
             array_output(array_sorted, n);
             break;
         }
-    } while (choice != 15);
+    } while (choice != 12);
 
     return 0;
 }
